e6edbb47e610249d3acf8681e40f30ca3ed30799
mrbrownfr <elouan.marron@gmail.com>
2022-02-21 18:17:55 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+//! # Code du projet de SI
+//! 
+//! Ce projet Rust sera appliqué sur un microcontrolleur au niveau système, ce qui permettra d'accéder rapidement et de façon sécurisée aux fonctions du robot.
+//! 
+//! Ce programme n'est pas disponible à la réutilisation jusqu'à la fin du projet. De plus, lors de sa mise en ligne sur le domaine public,
+//! toute réutilisation à des fins compétitives est formellement interdite.
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
